9090512d484f0a5e38bacd1ddb5d3882ed117349
Farhan082 <farhanul.islam@selise.ch>
2024-01-12 21:40:30 +0600

    add deleted folder for initialize

--- EcommerceWIthDotNet/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace EcommerceWIthDotNet.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
